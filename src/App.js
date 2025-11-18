fe1b6a9f07eb19b669260f50f16f26e9d50a98ab
Thomas Batista <139007458+thomasbatista@users.noreply.github.com>
2025-11-18 09:39:02 -0300

    react-router implementado

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
+import Home from './components/pages/Home'
+import Contact from './components/pages/Contact'
+import Company from './components/pages/Company'
+import NewProject from './components/pages/NewProject'
+
+import Container from './components/layout/Container';
+
 function App() {
   return (
     <div className="App">
-      <p>Budget</p>
+      <Router>
+        <div>
+          <Link to="/">Home</Link>
+          <Link to="/contact">Contato</Link>
+          <Link to="/company">Empresa</Link>
+          <Link to="/newproject">Projeto novo</Link>
+        </div>
+        <Container customClass="min-height">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/company" element={<Company />} />
+            <Route path="/newproject" element={<NewProject />} />
+          </Routes>
+        </Container>
+        <p>Footer</p>
+      </Router>
     </div>
   );
 }
